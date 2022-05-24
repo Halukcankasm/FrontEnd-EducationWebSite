8df9b902424a8e29138678a23878fad15acf3751
Halukcankasm <65433322+Halukcankasm@users.noreply.github.com>
2022-05-24 10:03:46 +0300

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,10 @@
+/*---------- Javascript for Toggle Menu ----------*/
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+	navLinks.style.width = "30%";
+}
+
+function hiddenMenu(){
+	navLinks.style.width = "0px";
+}
